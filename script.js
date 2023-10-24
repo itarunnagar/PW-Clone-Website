badb679c7640017160f474f0f7c4327967ecc67f
Tarun Nagar <itarunnagar@gmail.com>
2023-10-24 17:14:23 +0530

    Without JS

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const scroller = document.querySelectorAll("hero1");
-const autoScroller = () => {
-  hero1.scrollLeft += hero1.width;
-};
-setInterval(autoScroller, 2000);
+// Here  Header Section Start
+let menuBar = document.getElementsByClassName("menuBar");
+let magnifyingGlass = document.getElementsByClassName("magnifyingGlass");
+
+// Function of Menu Bar Click
+function menuBarClickable() {
+
+}
